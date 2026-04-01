eee1435b7247c6f4b0b84def93e14253bd70855d
CatchTheAlien <catchthealiengame@gmail.com>
2026-04-01 16:58:24 +0200

    Optimizaciones Iniciales

--- src/data/alien_types.ts
+++ src/data/alien_types.ts
@@ -22,6 +22,7 @@ export const ALIEN_TYPES: AlienType[] = [
     name: 'Slug',
     slug: 'slug',
     folder: 'slug',
+    modelUrl: 'https://makerworld.com/es/models/2602387-brain-slug#profileId-2871553'
   },
   {
     name: 'Greeter',
